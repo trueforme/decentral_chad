d26cd6569a8263cc02cdb995b0616cfd936d9d88
OK <143250301+OK219@users.noreply.github.com>
2024-08-20 15:01:46 +0500

    to_test

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import client
+import tkinter
+import threading
+def btt_opt(field):
+    text = field.get()
+    me.send_msgH(text)
 
-try:
-    with open('nickname.txt', 'r') as f:
-        nick = f.readline()
 
-except FileNotFoundError:
-    nick = input('Введите ник: ')
-    with open('nickname.txt', 'w') as f:
-        f.write(nick)
 
-me = client.Client(20122, nick)
-try:
-    me.Host(1)
-except OSError:
-    me.Connect()
+me = client.Client(20122,'me1')
+me.start_session("")
+root = tkinter.Tk()
+field = tkinter.Entry()
+field.place(x=0,y=0)
+btt = tkinter.Button(command=lambda x=field: btt_opt(x))
+btt.place(x=0,y=20)
+th = threading.Thread(target=me.get_msgH,args=[0])
+th.start()
+
+root.mainloop()
+
+
+
