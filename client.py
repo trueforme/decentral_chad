ceb8ff68fc3aa63e0c70ca4083accd1b5bbf532f
OK <143250301+OK219@users.noreply.github.com>
2024-09-05 18:01:24 +0500

    арбуз арбуз

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
 import socket
 import json
+import threading
 class Client():
     def __init__(self, port,nickname, max_clients=1):
         self.port = int(port)
         self.max_clients = max_clients
         self.nickname = nickname
         self.clients_ip = ['' for i in range(self.max_clients)]
-        self.clients_socket = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(self.max_clients)]
+        self.clients_socket = [socket.socket() for i in range(self.max_clients)]
+        self.threds = [threading.Thread() for i in range(self.max_clients)]
+        print(self.clients_socket[0])
         self.clients_nick = {}
-        self.host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host_socket.bind(('localhost',self.port))
+        self.host_socket = socket.socket()
+        self.host_socket.bind(('192.168.0.102',self.port))
+        print(self.host_socket)
         self.host_socket.listen(self.max_clients)
         self.clients_socket_busy = [False for i in range(self.max_clients)]
         # self.fill_client_data()
@@ -22,71 +26,52 @@ class Client():
                 return index
         return None
 
-    def start_session(self,ip):
+    def start_session(self,ip, port):
+        if ip in self.clients_ip:
+            print('уже подключен')
+            return
         client_index = self.get_free_socket_index()
         if client_index is None:
             print('error1')
         self.clients_ip[client_index] = ip
-        if not self.connect(client_index):
-            print('error2')
-        conn, addr = self.host_socket.accept()
+        print('there')
+        self.connect(client_index, port)
         print("sesion started")
 
 
-
-
-
-    def connect(self,client_index):
-        try:
-            self.clients_socket[client_index].connect((self.clients_ip[client_index], self.port))
+    def connect(self,client_index, port):
+        # try:
+            print(client_index,self.clients_ip,port)
+            self.clients_socket[client_index].connect((self.clients_ip[client_index], port))
             self.clients_socket_busy[client_index] = True
+            self.clients_nick[self.clients_ip[client_index]] = self.clients_socket[client_index].recv(1024).decode()
+            self.clients_socket[client_index].send(self.nickname.encode())
+
             return True
-        except:
-            return False
+        # except:
+        #     return False
+
+    def accept_connection(self):
+        while True:
+            index = self.get_free_socket_index()
+            if index is not None:
+                conn, addr = self.host_socket.accept()
+                if addr not in self.clients_ip:
+                    self.clients_ip[index] = addr
+                    self.clients_socket[index] = conn
+                    self.clients_socket_busy[index] = True
+                    self.clients_socket[index].send(self.nickname.encode())
+                    self.clients_nick[self.clients_ip[index]] = self.clients_socket[index].recv(1024).decode()
+                else:
+                    print('уже подключен')
 
 
-
-
-    # def fill_client_data(self):
-    #     try:
-    #         f = open('clients_data.txt', 'r')
-    #         for line in f.readlines():
-    #             ip,nick = line.split(': ')
-    #             self.clients_nick[ip] = nick
-    #         f.close()
-    #     except FileNotFoundError:
-    #         f = open('clients_data.txt', 'w')
-    #         f.close()
-
-    # def Host(self,max_clients):
-    #     self.socket.bind(('',self.port))
-    #     self.socket.listen(max_clients)
-    #     self.conn, self.addr = self.socket.accept()
-    #     nick = self.conn.recv(1024)
-    #     self.clients_nick[self.addr[0]] = nick.decode()
-    #     self.Update_clients_data()
-    #     data = json.dumps(self.clients_nick)
-    #     self.conn.send(data.encode())
-    #
-    # def Connect(self):
-    #     self.socket.connect(('localhost',self.port))
-    #     self.socket.send(self.nickname.encode())
-    #     self.clients_nick = json.loads(self.socket.recv(1024).decode())
-    #     self.Update_clients_data()
     def send_msgH(self,msg,ind):
         self.clients_socket[ind].send(msg.encode())
+        with open(f'{self.clients_ip[ind]}.txt','a+') as f:
+            f.write(f'{self.nickname}:{msg}\n')
     def get_msgH(self,ind):
         while True:
-            print(self.clients_socket[ind].recv(1024).decode())
-    #
-    #
-    #
-    # def Update_clients_data(self):
-    #     lines = []
-    #     for key,val in self.clients_nick.items():
-    #         lines.append(f'{key}: {val}')
-    #     with open('clients_data.txt','w') as f:
-    #         f.writelines(lines)
-
-
-
+            msg = self.clients_socket[ind].recv(1024).decode()
+            with open(f'{self.clients_ip[ind]}.txt','a+') as f:
+                f.write(f'{self.clients_nick[self.clients_ip[ind]]}:{msg}')
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import client
 import tkinter
 import threading
-def btt_opt(field):
-    text = field.get()
-    me.send_msgH(text)
+# def btt_opt(field):
+#     text = field.get()
+#     me.send_msgH(text)
 
 
 
-me = client.Client(20122,'me1')
-me.start_session("")
-root = tkinter.Tk()
-field = tkinter.Entry()
-field.place(x=0,y=0)
-btt = tkinter.Button(command=lambda x=field: btt_opt(x))
-btt.place(x=0,y=20)
-th = threading.Thread(target=me.get_msgH,args=[0])
-th.start()
+me = client.Client(20121,'me1')
+# th2 = threading.Thread(target=me.accept_connection)
+# th2.start()
+me.start_session('192.168.0.105',20122)
 
-root.mainloop()
+# root = tkinter.Tk()
+# field = tkinter.Entry()
+# field.place(x=0,y=0)
+# btt = tkinter.Button(command=lambda x=field: btt_opt(x))
+# btt.place(x=0,y=20)
+# th = threading.Thread(target=me.get_msgH,args=[0])
+# th.start()
+
+# root.mainloop()
 
 
 
