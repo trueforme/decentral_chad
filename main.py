c677b7f88d94bd3bccf824a46bf0b7f5f37ddcff
OK <143250301+OK219@users.noreply.github.com>
2024-09-09 23:06:33 +0500

    big msg fix and client rework

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import client
-import tkinter
-import threading
-# def btt_opt(field):
-#     text = field.get()
-#     me.send_msgH(text)
+import window
 
 
 
-me = client.Client(20121,'me1')
-# th2 = threading.Thread(target=me.accept_connection)
-# th2.start()
-me.start_session('192.168.0.105',20122)
+me = client.Client(20123,)
 
-# root = tkinter.Tk()
-# field = tkinter.Entry()
-# field.place(x=0,y=0)
-# btt = tkinter.Button(command=lambda x=field: btt_opt(x))
-# btt.place(x=0,y=20)
-# th = threading.Thread(target=me.get_msgH,args=[0])
-# th.start()
-
-# root.mainloop()
 
 
 
